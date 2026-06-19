bd7d102cdd87ba8c6e2a1865170e1f4328ef5e3a
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-19 15:08:27 +0545

    theme stored in local storage  +  actual theme logic using Context API

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,15 +1,44 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/footer";
+import { ThemeProvider } from "./components/Theme/ThemeContext";
+import { useEffect, useState } from "react";
+
+// import Layout from "./Layout"; 
+// you can use Layout component to wrap your app content, but in this case, we are using ThemeProvider directly in App component.
 
 function App() {
+const[themeMode,setThemeMode] = useState(
+  localStorage.getItem("theme") || "light"
+);
+
+const lightTheme = () => {
+  setThemeMode("light")
+}
+
+const darkTheme = () => {
+  setThemeMode("dark");
+}
+
+//actual change in theme
+
+useEffect(() => {
+  localStorage.setItem("theme", themeMode);
+  document.querySelector('html').classList.remove("light","dark");
+  document.querySelector('html').classList.add(themeMode);
+},[themeMode])
+
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
-  );
+    
+      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      {/* <Layout> */}
+        <Header />
+        <Outlet />
+        <Footer />
+      {/* </Layout> */}
+      </ThemeProvider>
+   
+    );
 }
 
 export default App;
