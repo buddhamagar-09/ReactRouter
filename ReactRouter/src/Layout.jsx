48c0442c9a6d56c63c1f215dda6ab8782a27eaf4
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-19 15:10:18 +0545

    common styles for the children components for consistency

---
+++ ReactRouter/src/Layout.jsx
@@ -0,0 +1,12 @@
+
+
+// export default function Layout({ children }) {
+//   return (
+//     <div className="flex flex-col min-h-screen bg-white dark:bg-slate-900 text-black dark:text-white transition-colors duration-300">
+//         {children}
+//     </div>
+//   )
+// }
+
+// it is used in App.jsx to wrap the entire application, providing a consistent layout and styling for all pages. The children prop allows different components to be rendered within this layout, maintaining a uniform look and feel across the app.
+// it helps to manage the dark mode and light mode themes by applying the appropriate classes to the root div, ensuring that the entire application responds to theme changes consistently.
