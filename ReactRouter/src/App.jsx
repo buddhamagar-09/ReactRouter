470ede5dd8c8e4019633da5a8834e0c4dd511740
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-21 20:48:52 +0545

    code structured

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -4,41 +4,38 @@ import Footer from "./components/Footer/footer";
 import { ThemeProvider } from "./components/Theme/ThemeContext";
 import { useEffect, useState } from "react";
 
-// import Layout from "./Layout"; 
+// import Layout from "./Layout";
 // you can use Layout component to wrap your app content, but in this case, we are using ThemeProvider directly in App component.
 
 function App() {
-const[themeMode,setThemeMode] = useState(
-  localStorage.getItem("theme") || "light"
-);
+  const [themeMode, setThemeMode] = useState(
+    localStorage.getItem("theme") || "light",
+  );
 
-const lightTheme = () => {
-  setThemeMode("light")
-}
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
 
-const darkTheme = () => {
-  setThemeMode("dark");
-}
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
 
-//actual change in theme
-
-useEffect(() => {
-  localStorage.setItem("theme", themeMode);
-  document.querySelector('html').classList.remove("light","dark");
-  document.querySelector('html').classList.add(themeMode);
-},[themeMode])
+  //actual change in theme
+  useEffect(() => {
+    localStorage.setItem("theme", themeMode);
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    
-      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       {/* <Layout> */}
-        <Header />
-        <Outlet />
-        <Footer />
+      <Header />
+      <Outlet />
+      <Footer />
       {/* </Layout> */}
-      </ThemeProvider>
-   
-    );
+    </ThemeProvider>
+  );
 }
 
 export default App;
