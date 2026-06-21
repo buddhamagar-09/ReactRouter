ae5db1505a3848963aa71957ac307a9fc5305121
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-21 20:47:49 +0545

    updated

--- ReactRouter/src/components/Header/Header.jsx
+++ ReactRouter/src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
               onClick={() =>
                 themeMode === "light" ? darkTheme() : lightTheme()
               }
-              className="text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-slate-800 focus:ring-4 focus:ring-gray-300 dark:focus:ring-slate-600 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 transition-colors duration-300"
+              className="text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-slate-800 focus:ring-4 focus:ring-gray-300 dark:focus:ring-slate-600 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 transition-colors duration-300"
             >
               {themeMode === "light"
                 ? "🌙 Dark Mode"
@@ -27,7 +27,7 @@ export default function Header() {
 
             <Link
               to="#"
-              className="text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-slate-800 focus:ring-4 focus:ring-gray-300 dark:focus:ring-slate-600 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 transition-colors duration-300"
+              className="text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-slate-800 focus:ring-4 focus:ring-gray-300 dark:focus:ring-slate-600 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 transition-colors duration-300"
             >
               Log in
             </Link>
